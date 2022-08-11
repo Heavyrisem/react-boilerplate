9459e2333b323f7873d5fe72668de8ed47012831
heavyrisem <heavyrisem@gmail.com>
2022-08-11 20:52:01 +0900

    feat: Setup React BoilerPlate

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# react-boilerplate
+# React BoilerPlate
+
+## Description
+
+this is a boilerplate for react projects
+
+- Vite
+- ESlint
+- Prettier
+- Tailwind
+- Twin.Macro
+- Docker
+- Docker-Compose
+
+## Installation
+
+```bash
+$ git clone
+$ yarn install
+```
+
+## Usage
+
+```bash
+$ yarn dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-macros',
+          [
+            '@emotion/babel-plugin-jsx-pragmatic',
+            {
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react',
+            },
+          ],
+          ['@babel/plugin-transform-react-jsx', { pragma: '__cssprop' }, 'twin.macro'],
+        ],
+      },
+    }),
+    tsconfigPaths(),
+    eslint({ emitWarning: true, include: 'src/**/*.{ts,tsx}' }),
+  ],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: 'src' },
+      { find: '@components', replacement: 'src/components' },
+      { find: '@pages', replacement: 'src/pages' },
+      { find: '@hooks', replacement: 'src/hooks' },
+    ],
+  },
+});
